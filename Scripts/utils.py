ed2cff7768c4a64a8b577c1414653de9788ccc07
Raphael <r.charbey@gmail.com>
2018-01-24 14:38:34 +0100

    add utils

---
+++ Scripts/utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 12:03:23 2018
+
+@author: raphael
+"""
+
+from os import listdir
+
+DATA = '../Data/Representativities/'
+
+def compute_list_egos():
+    list_egos = [ego.split('_')[0] for ego in listdir(DATA) if 'k4' in ego]
+    return list_egos
+
+LIST_EGOS = compute_list_egos()
+POS = range(4,15)
