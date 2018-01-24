318ce3fb715f90831ef4796094be9abd58484a5d
Raphael <r.charbey@gmail.com>
2018-01-24 15:15:57 +0100

    random_sample

--- Scripts/kmeans.py
+++ Scripts/kmeans.py
@@ -7,15 +7,18 @@ Created on Wed Jan 24 12:02:45 2018
 """
 
 import csv
-import random
 import pandas as pd
 from sklearn.metrics import silhouette_samples
 from sklearn.cluster import KMeans
 import numpy as np
+import os
+import random
 
 from utils import LIST_EGOS
 
 random.seed()
+LIST_EGOS = random_sample(LIST_EGOS, 50)
+
 DATA = '../Data/Representativities/'
 
 ORBITS = range(5, 16)
@@ -77,6 +80,7 @@ def get_classe_representativity(list_of_alters):
 
 def kmeans_all(data):
     for k_value in K_VALUES :
+        
         s_max = 0
         labels_max = False
         for i in range(100):
@@ -87,7 +91,9 @@ def kmeans_all(data):
                 labels_max = kmeans.labels_
                 s_max = s
             
-        directory = '../Results/KMeans/Typo_%s' % k_value
+        ref_ego = EGO_PER_ALTER.value()[0]
+        directory = '../Results/KMeans/Typo_%s/%s' % (k_value, ref_ego)
+        os.mkdir(directory)
         alters_par_classe = {}
         
         members_per_cluster = [[labels_max == k] for k in range(k_value)]
@@ -98,12 +104,12 @@ def kmeans_all(data):
                 alters_par_classe[k] = []
                 with open('%s/alters_classe_%s.csv' % (directory, k), 'w') as to_write:
                     csv_w = csv.writer(to_write, delimiter = ';')
-                    csv_w.writerow(['ego_id', 'alter_id'] + [x+1 for x in POS])
+                    csv_w.writerow(['ego_id', 'alter_id'] + ORBITS)
                     for i in range(len(ALTERS)):
                         if my_members[i]:
                             alter = ALTERS[i]
                             alters_par_classe[k].append(alter)
-                            csv_w.writerow([EGO_PER_ALTER[alter], alter] + [REPR_PER_ALTER[alter][j] for j in [x+1 for x in POS]])
+                            csv_w.writerow([EGO_PER_ALTER[alter], alter] + [REPR_PER_ALTER[alter][orbit] for orbit in ORBITS])
 
         repr_par_classe= {}
         for classe in alters_par_classe:
@@ -111,7 +117,7 @@ def kmeans_all(data):
             
         with open('%s/kmeans_stats.csv' % directory, 'w') as to_write:
             csv_w = csv.writer(to_write, delimiter = ';')
-            csv_w.writerow(['classe', 'nb', 'prop'] + [""] + [x+1 for x in POS])
+            csv_w.writerow(['classe', 'nb', 'prop'] + [""] + ORBITS)
             classe_par_taille = [classe for classe in alters_par_classe]
             classe_par_taille.sort(key = lambda classe : len(alters_par_classe[classe]))
             for classe in classe_par_taille:
@@ -119,15 +125,14 @@ def kmeans_all(data):
                 prop = round(nb/float(len(ALTERS)), 2)
                 
                 row = [classe, nb, prop] + [""]
-                for key in [x+1 for x in POS]:
-                    row.append(repr_par_classe[classe][key])                    
+                for orbit in ORBITS:
+                    row.append(repr_par_classe[classe][orbit])                    
                 csv_w.writerow(row)      
 
         print 'silhouette k = %s : %s' % (k_value, round(s_max,3))  
         print kmeans.cluster_centers_
         
         
-corpus = 'facebook'
 k = 5
 K_VALUES = [5]
 
